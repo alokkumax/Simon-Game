82ad1c33ac24fa225250bc705afdf254f9d16562
alokkumax <alokkumax1@gmail.com>
2023-08-05 20:05:59 +0530

    added fncs

--- script.js
+++ script.js
@@ -1,87 +1,78 @@
 let levelAr = ["green", "red", "yellow", "blue"];
-let level = 1;
+let level = 0;
 let gameAr = [];
 let inputAr = [];
+var started = false;
 
-function startGame(){
-    loadBtn();
-    $("body").keypress(function(){
+$("body").keypress(function(){
+    if(!started){
         $("h1").html("Level "+ level)
         randomLevel();
-    })
-    }
-function loadBtn(){
-    $("button").click(function(){
-        
-        let curr = $(this).attr("id")
-        var audio = new Audio("/sounds/"+curr+".mp3");
-        audio.play();
-        // first we want btns to be disabled.
-        inputAr.push(curr)
-        console.log(inputAr)
-        if(gameAr.length>= 1){
-           
-            if(gameAr.toString() === inputAr.toString()){
-                console.log("passed");
-                // flushRestart();
-
-                randomLevel();
-            }else{
-                console.log("go");
-                gameOver();
-                setTimeout(function(){
-                    $("body").removeClass("go");
-                    gameAr = []
-                    inputAr = []
-                },"1000")
-
-            }
+        started = true;
         }
     })
+    
+ $("button").click(function(){
+        let curr = $(this).attr("id")
+        inputAr.push(curr)
+
+        //play
+        playSound(curr);
+
+        //animate
+
+        //check
+        checkRes(inputAr.length-1);
+})
+
+function checkRes(currLevel){
+    if(gameAr[currLevel] === inputAr[currLevel]){
+        if(inputAr.length === gameAr.length){
+            setTimeout(function () {
+                randomLevel();
+              }, 1000);
+        }
+    }else{
+        //play sound
+        playSound("wrong");
+        $("body").addClass("go");
+        $("h1").text("Game Over, Press S Key to Restart");
+        $("h3").addClass("kpop")
+        $("h3").text("Your Score: "+level)
+        //setTIme
+        setTimeout(function () {
+            $("body").removeClass("go");
+            $("h3").removeClass("kpop")
+
+          }, 2000);
+        //startOver
+        gameOver();
+    }
 }
-// checkForWin()
-// function flushRestart(){
-//     inputAr = [];
-//     let i = 0;
-//     while(gameAr.toString() === inputAr.toString()){
-//         $("button").click(function(){
-//         let curr = $(this).attr("id")
-//         var audio = new Audio("/sounds/"+curr+".mp3");
-//         audio.play();
-
-//         inputAr.push(curr)
-//         console.log(inputAr)
-
-//         })
-//         i++;
-//     }
-// }
 
 function randomLevel(){
+    inputAr=[];
+    level++;
+    $("h1").html("Level "+ level)
+
     let randomC = levelAr[Math.floor(Math.random()*4)];
     $("#"+randomC).addClass("onP");
     gameAr.push(randomC);
-    console.log(gameAr)
+
     setTimeout(function(){
         $("#"+randomC).removeClass("onP");
     },"1000")
+
+    playSound(randomC);
 }
-function updateLevel(){
-    // event thru
-}
-function wrongPlay(){
-    $("body").css("background-color" , "red");
-    var audio = new Audio("/sounds/wrong.mp3");
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
-
-
-
 function gameOver(){
    
-        $("h1").html("Game Over, Press Any Key to Restart")
-        $("body").addClass("go")
-        var audio = new Audio("/sounds/wrong.mp3");
-        audio.play();
+     level = 0;
+     gameAr = []
+     started = false;
     
 }
